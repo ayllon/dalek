291f6c29b7058057e19352bd525bc6698807481b
alexcomps <alexcomps@b4c5d946-44fe-48fd-ba25-f62a37739153>
2009-02-02 16:10:08 +0000

    Keyboard handler and keymap improved.
    git-svn-id: http://svn.code.sf.net/p/litoxds/code/trunk@11 b4c5d946-44fe-48fd-ba25-f62a37739153

--- bootloader/stage2/keyboard.c
+++ bootloader/stage2/keyboard.c
@@ -14,7 +14,10 @@ static struct
 {
   uint8 shift, caps;
   uint8 numlock;
-}kb_flags = {0, 0, 0};
+  uint8 lcontrol, lalt;
+  uint8 rcontrol, ralt;
+  uint8 escaped;
+}kb_flags = {0, 0, 0, 0, 0, 0, 0, 0};
 
 /* Keymap */
 extern uint16 kb_keymap[], kb_shift_keymap[], kb_altgr_keymap[];
@@ -36,45 +39,86 @@ void kb_handler(struct regs *regs)
 
   switch(c)
   {
-  case 0x2A:
+    // Escaped
+  case 0xE0:
+    kb_flags.escaped = 1;
+    break;
+    // Mod keys
+  case 0x2A: case 0x36: // Shift down
     kb_flags.shift = 1;
     break;
-  case 0xAA:
+  case 0xAA: case 0xB6: // Shift up
     kb_flags.shift = 0;
     break;
+  case 0x1D: // Control down
+    if(kb_flags.escaped)
+      kb_flags.rcontrol = 1;
+    else
+      kb_flags.lcontrol = 1;
+    break;
+  case 0x9D: // Control up
+    if(kb_flags.escaped)
+      kb_flags.rcontrol = 0;
+    else
+      kb_flags.lcontrol = 0;
+    break;
+  case 0x38: // Alt down
+    if(kb_flags.escaped)
+      kb_flags.ralt = 1;
+    else
+      kb_flags.lalt = 1;
+    break;
+ case 0xB8: // Alt up
+    if(kb_flags.escaped)
+      kb_flags.ralt = 0;
+    else
+      kb_flags.lalt = 0;
+    break;
+  case 0x3A: // Shift lock down
+    kb_flags.caps = !kb_flags.caps;
+    break;
+    // Default
   default:
     if(c & 0x80)
     {
-      // Breakcode
+      // Breakcode (key up)
     }else
     {
       // Get char
       if(kb_flags.shift)
 	key = kb_shift_keymap[c];
+      else if(kb_flags.ralt)
+	key = kb_altgr_keymap[c];
       else
 	key = kb_keymap[c];
 
-      // Shift block
-      if(kb_flags.caps)
+      // Check
+      if(key)
       {
-	if(key > 'a' && key < 'z')
-	  key = key - 'a' + 'A';
-	else if(key > 'A' && key < 'Z')
-	  key = key - 'Z' + 'a';
-      }
+	// Shift block
+	if(kb_flags.caps)
+	{
+	  if(key >= 'a' && key <= 'z')
+	    key = key - 'a' + 'A';
+	  else if(key >= 'A' && key <= 'Z')
+	    key = key - 'A' + 'a';
+	}
 
-      // Push into buffer
-      if((kb_buffer.push_index + 1) % KB_BUFFER_LEN != kb_buffer.pop_index)
-      {
-	kb_buffer.key[kb_buffer.push_index] = key;
-	kb_buffer.push_index = (kb_buffer.push_index + 1) % KB_BUFFER_LEN;
+	// Push into buffer if it isn't full
+	if((kb_buffer.push_index + 1) % KB_BUFFER_LEN != kb_buffer.pop_index)
+	{
+	  kb_buffer.key[kb_buffer.push_index] = key;
+	  kb_buffer.push_index = (kb_buffer.push_index + 1) % KB_BUFFER_LEN;
+	}
       }else
       {
-	// Buffer full. Ignore character :-(
-	return;
+	// Null char. Unknown
+	printf("[kb_handler()] Unknown scancode: 0x%x\n", c);
       }
     }
   }
+  // Reset escaped
+  kb_flags.escaped = (c == 0xE0);
 }
 
 /* kb_init()
--- bootloader/stage2/keymaps/es_es.c
+++ bootloader/stage2/keymaps/es_es.c
@@ -11,32 +11,36 @@
 
 uint16 kb_keymap[] = 
   {
-    NULL,
-    KEY_ESC,
-    '1' , '2', '3', '4', '5', '6', '7', '8', '9', '0', '\'', 0xAD, '\b',
-    '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '`', '+',
-    KEY_ENTER,
-    KEY_LCONTROL,
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 0xA4, '\'', 0x87,
-    KEY_LSHIFT,
-    '<',
-    'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '-',
-    KEY_RSHIFT,
-    NULL,
-    KEY_LALT,
-    ' ',
-    KEY_CAPS,
-    KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10,
-    KEY_NUMLOCK, KEY_SCROLL,
-    '7', '8', '9',
-    '-',
-    '4', '5', '6',
-    '+',
-    '1', '2', '3',
-    '0', '.',
-    NULL, NULL, NULL,
-    KEY_F11, KEY_F12
-    // There are more
+    NULL,    // 0x00
+    KEY_ESC, //0x01
+    '1' , '2', '3', '4', '5', '6', '7', '8', '9', '0', '\'', 0xAD, '\b', // 0x0E
+    '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '`', '+',    // 0x1B
+    KEY_ENTER,    // 0x1C
+    KEY_LCONTROL, // 0x1D
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 0xA4, '\'', 0xA7, // 0x29
+    KEY_LSHIFT, // 0x2A
+    0x87,       // 0x2B
+    'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '-', // 0x35
+    KEY_RSHIFT, // 0x36
+    NULL,       // 0x37
+    KEY_LALT,   // 0x38
+    ' ',        // 0x39
+    KEY_CAPS,   // 0x3a
+    KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, // 0x44
+    KEY_NUMLOCK, KEY_SCROLL, // 0x46
+    '7', '8', '9',    // 0x49
+    '-',              // 0x4a
+    '4', '5', '6',    // 0x4d
+    '+',              // 0x4e
+    '1', '2', '3',    // 0x51
+    '0', '.',         // 0x53
+    NULL, NULL, '<',  // 0x56
+    KEY_F11, KEY_F12, // 0x58
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, // 0x5F
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, // 0x67
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, // 0x6F
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, // 0x77
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL  // 0x7F
     };
 
 uint16 kb_shift_keymap[] = 
@@ -47,9 +51,9 @@ uint16 kb_shift_keymap[] =
     '\t', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '^', '*',
     KEY_ENTER,
     KEY_LCONTROL,
-    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 0xA5, ' ', 0x80,
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 0xA5, ' ', 0xA6,
     KEY_LSHIFT,
-    '>',
+    0x80,
     'Z', 'X', 'C', 'V', 'B', 'N', 'M', ';', ':', '_',
     KEY_RSHIFT,
     NULL,
@@ -64,12 +68,16 @@ uint16 kb_shift_keymap[] =
     '+',
     '1', '2', '3',
     '0', '.',
-    NULL, NULL, NULL,
-    KEY_F11, KEY_F12
-    // There are more
+    NULL, NULL, '>',  // 0x56
+    KEY_F11, KEY_F12, // 0x58
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, // 0x5F
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, // 0x67
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, // 0x6F
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, // 0x77
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL  // 0x7F
     };
 
-uint16 kb_altgr_map[] = 
+uint16 kb_altgr_keymap[] = 
   {
     NULL,
     KEY_ESC,
@@ -77,9 +85,9 @@ uint16 kb_altgr_map[] =
     '\t', '@', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', '[', ']',
     KEY_ENTER,
     KEY_LCONTROL,
-    0x91, 0xE1, 'd', 'f', 'g', 'h', 'j', 'k', 'l', '~', '{', '}',
+    0x91, 0xE1, 'd', 'f', 'g', 'h', 'j', 'k', 'l', '~', '{', '\\',
     KEY_LSHIFT,
-    '|',
+    '}',
     'z', 'x', 'c', '"', '"', 'n', 'm', ';', 0xAB, '-',
     KEY_RSHIFT,
     NULL,
@@ -94,8 +102,12 @@ uint16 kb_altgr_map[] =
     '+',
     '1', '2', '3',
     '0', '.',
-    NULL, NULL, NULL,
-    KEY_F11, KEY_F12
-    // There are more
+    NULL, NULL, '<',  // 0x56
+    KEY_F11, KEY_F12, // 0x58
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, // 0x5F
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, // 0x67
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, // 0x6F
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, // 0x77
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL  // 0x7F
     };
 
