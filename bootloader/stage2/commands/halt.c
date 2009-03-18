d7680daca8954a3f044f03db2b4334ff1d2fe096
alexcomps <alexcomps@b4c5d946-44fe-48fd-ba25-f62a37739153>
2009-03-18 11:41:15 +0000

    It does nothing (still)
    git-svn-id: http://svn.code.sf.net/p/litoxds/code/trunk@21 b4c5d946-44fe-48fd-ba25-f62a37739153

--- bootloader/stage2/commands/halt.c
+++ bootloader/stage2/commands/halt.c
@@ -4,9 +4,19 @@
  */
 #include <printf.h>
 #include <cli.h> 
+#include <strings.h>
 
+// Try to power off the computer
+// We may keep working if we can't shut down
 uint8 halt(uint8 argn, const char **argv)
 {
-  printf("Not implemented :-(\n");
+  // Help
+  if(argn > 1 && strcmp(argv[1], "help"))
+  {
+    printf("Shut down the system if it is possible\n");
+    printf("Usage: halt [help]\n");
+    return 0;
+  }
+
   return 1;
 }
