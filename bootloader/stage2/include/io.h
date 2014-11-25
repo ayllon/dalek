7db06a969899cd8ec2d50a217e390db614427463
Alejandro Álvarez Ayllón <a.alvarezayllon@gmail.com>
2014-11-25 20:21:10 +0100

    Forgot self in io

--- bootloader/stage2/include/io.h
+++ bootloader/stage2/include/io.h
@@ -9,19 +9,20 @@
 
 #include <types.h>
 
-typedef struct
+typedef struct IODevice IODevice;
+struct IODevice
 {
   // Attributes
   char name[16];
   char description[32];
   void *data;
   // Methods
-  off_t   (*seek)(off_t offset, int whence);
-  ssize_t (*read)(void* buffer, size_t nbytes);
-  ssize_t (*write)(void* buffer, size_t nbytes);
-  int     (*ioctl)(unsigned long request);
+  off_t   (*seek)(IODevice* self, off_t offset, int whence);
+  ssize_t (*read)(IODevice* self, void* buffer, size_t nbytes);
+  ssize_t (*write)(IODevice* self, void* buffer, size_t nbytes);
+  int     (*ioctl)(IODevice* self, unsigned long request);
 
-}IODevice;
+};
 
 /**
  * Register method
